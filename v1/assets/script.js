e22db81adf6fb06f3875a64162a2e3cffc803dd5
Sooyeon <sooyeonoh48@gmail.com>
2021-07-03 14:16:55 -0700

    Revert "Delete v1 directory"
    This reverts commit f8e372f2f5cb10648f373dc216992e36616da5d0.

---
+++ v1/assets/script.js
@@ -0,0 +1,39 @@
+let panel = document.querySelector('#sliding-panel');
+let navLinks = document.querySelector('.nav-links');
+let navBarToggle = document.querySelector('#nav-toggle');
+let section = document.querySelector('.wrapper');
+let link = document.querySelectorAll('header a');
+
+navBarToggle.addEventListener('click', event => {
+    navLinks.classList.toggle('active');
+    if (navLinks.classList.contains('active')) {
+        slidePanelDown();
+    }
+    else {
+        slidePanelUp();
+    }
+});
+
+section.addEventListener('click', event => {
+    if (navLinks.classList.contains('active')) {
+        navLinks.classList.toggle('active');
+        slidePanelUp();
+    }
+});
+
+link.forEach(item => {
+    item.addEventListener('click', event => {
+        if (!navLinks.classList.contains('active')) {
+            navLinks.classList.toggle('active');
+            slidePanelUp();
+        }
+    })
+});
+
+function slidePanelDown() {
+    panel.style.top = '0';
+}
+
+function slidePanelUp() {
+    panel.style.top = '-170px';
+}
